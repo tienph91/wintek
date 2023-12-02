c1ee70324ed96da34292629c7d5308c78c39132f
tienph <hoangtien.cp@gmail.com>
2023-12-02 13:36:39 +0700

    initial project

---
+++ src/main/java/com/wintek/brightcolor/BrightcolorApplication.java
@@ -0,0 +1,13 @@
+package com.wintek.brightcolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightcolorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightcolorApplication.class, args);
+	}
+
+}
